909532d458a61a7a5997c5668337b1a69d65877b
unknown <edfournier@umass.edu>
2024-05-16 13:24:37 -0400

    local storage set and remove

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,27 @@
-const testButton = document.getElementById("test-button");
-testButton.addEventListener("click", () => {
-    console.log("clicked");
+const save = document.getElementById("save");
+const form = document.getElementById("form");
+const reset = document.getElementById("reset");
+
+async function savePreferences() {
+    const formData = new FormData(form);
+    await chrome.storage.local.set(formData.entries().reduce((state, entry) => {
+        state[entry[0]] = entry[1];
+        return state;
+    }, {}));
+}
+
+async function resetPreferences() {
+    chrome.storage.local.clear();
+    form.reset();
+}
+
+save.addEventListener("click", saveFormData);
+reset.addEventListener("click", resetPreferences);
+
+// TODO: 
+/*
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { greeting: "Hello from popup!" });
 });
+*/
+
