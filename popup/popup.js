d98e14123c23df8e3096a54e34edbab2a084215f
unknown <edfournier@umass.edu>
2024-05-17 20:27:03 -0400

    connect content and popup scripts

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,12 @@ const save = document.getElementById("save");
 const form = document.getElementById("form");
 const reset = document.getElementById("reset");
 
+save.addEventListener("click", saveSettings);
+reset.addEventListener("click", resetSettings);
+
+/*
+ * Saves user settings to local storage and notifies content script
+ */
 async function saveSettings() {
     const formData = new FormData(form);
     await chrome.storage.local.set(formData.entries().reduce((state, entry) => {
@@ -10,18 +16,24 @@ async function saveSettings() {
     }, {}));
 }
 
+/*
+ * Resets user settings to defaults and nukes local storage
+ */ 
 async function resetSettings() {
     chrome.storage.local.clear();
     form.reset();
+
+    // Send message to content script
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { greeting: 'hello' });
+    });
 }
 
-save.addEventListener("click", saveSettings);
-reset.addEventListener("click", resetSettings);
-
-// TODO: 
 /*
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { greeting: "Hello from popup!" });
-});
-*/
-
+ * Sends given message to content script
+ */
+function sendMessage(message) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, message);
+    });
+}
