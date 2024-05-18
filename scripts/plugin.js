0f05174f89e0b673b194ae55bc781b1006a3b2a4
unknown <edfournier@umass.edu>
2024-05-18 18:21:44 -0400

    cleanup replac code

--- scripts/plugin.js
+++ scripts/plugin.js
@@ -1,11 +1,12 @@
 // Site's window properties
-const mq = window.Desmos.MathQuill.MathField(
+const mathField = window.Desmos.MathQuill.MathField(
     document.getElementsByClassName("dcg-math-field dcg-focus dcg-mq-editable-field dcg-mq-math-mode")[0]
 );	
 const calc = window.Calc;
 
 // Recieve order to execute or state updates from content script
-document.addEventListener("dmb-run-hex", run);  
+document.addEventListener("dmb-run-bin", () => run(/0b_\{[0-1]+\}/g));  
+document.addEventListener("dmb-run-hex", () => run(/0x([A-Fa-f]|_\{[0-9]+\})+/g));  
 document.addEventListener("dmb-update", (e) => {
     console.log("New settings: " + e.detail);
 });
@@ -27,20 +28,12 @@ function toDecimal(string) {
     return num; 
 }
 
-function replace(latex) {
-    const pattern = /0x(?:[A-Fa-f]|_\{\d+\})+/g;
-    return latex.replace(pattern, (match) => {
-        const clean = match.replace(/[_{}]/g, "");
-        return toDecimal(clean).toString()
-    });
-}
-
-/*
- * Replace hex literals with decimal in web page
- */ 
-function run() {
-    console.log("Running...");
+function run(pattern) {
     const state = calc.getState();
-    state.expressions.list[0].latex = replace(mq.latex())  // TODO: not always 0
+    // TODO: list[0] is only first expression?
+    state.expressions.list[0].latex = mathField.latex().replace(pattern, (match) => {
+        const clean = match.replace(/[_{}]/g, "");
+        return toDecimal(clean).toString();
+    });
     calc.setState(state);
 }
