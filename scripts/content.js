58b747542fd6591ee4a4e41f6b06b24427ecf830
unknown <edfournier@umass.edu>
2024-05-17 20:26:26 -0400

    add run functionality

--- scripts/content.js
+++ scripts/content.js
@@ -4,22 +4,19 @@ script.setAttribute("type", "module");
 script.setAttribute("src", `chrome-extension://${chrome.runtime.id}/scripts/plugin.js`);
 document.head.appendChild(script);
 
-function sendUpdate(state) {
-    document.dispatchEvent(new CustomEvent("dmb-content-update", {
-        detail: {
-            state: state
-        }
+function sendEvent(event, detail) {
+    document.dispatchEvent(new CustomEvent(event, {
+        detail: detail
     }));
 }
 
-
-// TODO: Setup content-popup communication w/ chrome.runtime.sendMessage
-chrome.runtime.onMessage.addListener((message, send, sendResponse) => {
-
+// Recieve messages from popup script
+chrome.runtime.onMessage.addListener((message) => {
+    console.log('Message received in content script:', message);
+    sendEvent("dmb-run");
 });
 
 function content() {
-    sendUpdate("test sent from content script");
 }
 
 // Wait for injected script to attach listener
--- scripts/plugin.js
+++ scripts/plugin.js
@@ -1,23 +1,15 @@
-// Recieve messages from content script
-document.addEventListener("dmb-content-update", (e) => {
-    console.log("content.js recieved: " + e.detail.state);
+// Recieve order to execute or state updates from content script
+document.addEventListener("dmb-run", run);  
+document.addEventListener("dmb-update-settings", (e) => {
+    console.log("New settings: " + e.detail);
 });
 
-const mathField = document.getElementsByClassName(
-    "dcg-math-field dcg-focus dcg-mq-editable-field dcg-mq-math-mode"
-    )[0];
-const mq = window.Desmos.MathQuill.MathField(mathField);	
-const calc = window.Calc;
-
-// Manipulate state
-mq.latex("1 + 2")
-const state = calc.getState();
-state.expressions.list[0].latex = mq.latex()	// Not always 0. 
-calc.setState(state);
-
-function convert(hex) { 
-    const n = 4;
-    let num = Number(hex); 
+/*
+ * Convert n-bit binary or hex string to decimal
+ */
+function toDecimal(string) {
+    const n = 64;
+    let num = Number(string); 
     // Sign extend
     if ((num >> (n - 1)) & 1) {
         num |= -1 << n;
@@ -25,6 +17,9 @@ function convert(hex) {
     return num; 
 }
 
+/*
+ *
+ */
 function extractHex(latex) {
     const result = [];
     for (let i = latex.indexOf("0x"); i != -1; i = latex.indexOf("0x", i)) {
@@ -35,9 +30,26 @@ function extractHex(latex) {
         while (latex.charAt(j - 1) === '}' && latex.charAt(j - 2) === '}') {
             j--;
         }
-        token = latex.substring(i, j);
-        result.push([token, token.replace(/[^x0-9a-fA-F]/g, "")]);
+        const token = latex.substring(i, j);
+        result.push({token: token, stripped: token.replace(/[^x0-9a-fA-F]/g, "")});
         i = j;
     }
     return result;
+}
+
+const mathField = document.getElementsByClassName(
+    "dcg-math-field dcg-focus dcg-mq-editable-field dcg-mq-math-mode"
+    )[0];
+const mq = window.Desmos.MathQuill.MathField(mathField);	
+const calc = window.Calc;
+
+function run() {
+    let latex = mq.latex();
+    extractHex(latex).forEach((hex) => {
+        latex = latex.replace(hex.token, toDecimal(hex.stripped));
+    });
+    const state = calc.getState();
+    mq.latex(latex);
+    state.expressions.list[0].latex = mq.latex();  // TODO: not always 0
+    calc.setState(state);
 }
